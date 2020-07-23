dd66204d3962153569d99d7932f2c0f6c913ea9a
nckz <nick.zwart@gmail.com>
2020-07-23 11:35:35 -0400

    added ale and shellcheck

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,12 @@ MAINTAINER Nick Zwart <dr.nicky.z@gmail.com>
 RUN apk --update add --no-cache \
     bash \
     vim \
+    shellcheck \
     git
+
+# install https://github.com/dense-analysis/ale
+RUN mkdir -p ~/.vim/pack/git-plugins/start && \
+    git clone --depth 1 https://github.com/dense-analysis/ale.git ~/.vim/pack/git-plugins/start/ale
 
 # copy the entrypoint script
 COPY entrypoint /
--- config.bash
+++ config.bash
@@ -11,15 +11,15 @@ function config {
 # make the docker tags for this project
 DOCKER_ACC='3x3t3r'
 DOCKER_REP='ni'
-DOCKER_TAG='latest'
-DOCKER_REF="${DOCKER_ACC}/${DOCKER_REP}"
+export DOCKER_TAG='latest'
+export DOCKER_REF="${DOCKER_ACC}/${DOCKER_REP}"
 
 # Get the directory of THIS script. Assume its located in the project root.
 SWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PROJECT_ROOT="$SWD"
 
 # install files and targets
-CMD_SRC="${PROJECT_ROOT}/${DOCKER_REP}"
-CMD_DEST="/usr/local/bin/${DOCKER_REP}"
+export CMD_SRC="${PROJECT_ROOT}/${DOCKER_REP}"
+export CMD_DEST="/usr/local/bin/${DOCKER_REP}"
 
 }
